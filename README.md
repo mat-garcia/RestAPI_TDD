12e27a018bd2a79aac411eef5a844659711870d1
Mateus Garcia <mateussoap@gmail.com>
2023-07-03 22:28:46 -0300

    upload project

---
+++ README.md
@@ -0,0 +1,1 @@
+# NodeJS Rest API - Aplicando TDD e Clean Architecture
---
+++ src/infra/database.js
@@ -0,0 +1,12 @@
+const pgp = require('pg-promise')();
+
+
+const db = pgp({
+  user: 'postgres',
+  password: '1234',
+  host: 'localhost',
+  port: 5432,
+  database: 'node'
+});
+
+module.exports = db;
